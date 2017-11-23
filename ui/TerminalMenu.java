67c050e473208a758c55a69adb1e707ed2d8e009
Chris <Chris.marquez@hotmail>
2017-11-22 16:59:20 -0800

    Refactor TerminalMenu.

--- ui/TerminalMenu.java
+++ ui/TerminalMenu.java
@@ -54,52 +54,37 @@ public class TerminalMenu {
     private void runChoice() throws SQLException {
         switch (this.choice) {
             case "1":
-                // query 1
-                System.out.println("Query 1 Results");
-                displayResults.display(sqlQueries.displaySchedule("Pomona", "Walnut", "1-1-2001"));     break;
-            case "2":
-                // query 2.a
-                System.out.println("Query 2 Results");
-                displayResults.displayTwo(sqlQueries.deleteTrip("Trip1","1-1-2001","MON 10:00AM"));
+                queryOne();
                 break;
+            case "2":
+                queryTwo();
+                 break;
             case "3":
-                //query 2.b
-                displayResults.displayThree(sqlQueries.addTrip("Trip1","11-21-2017","MON 10:55AM","MON 12:00PM","Driver1","Bus2"));  break;
+                queryThree();
+                break;
             case "4":
-                //query 2.c
-                displayResults.displayFour(sqlQueries.changeDriver("Driver1","Trip10","1-10-2001","SUN 10:00AM"));
+                queryFour();
                 break;
             case "5":
-                //query 2.d
-                displayResults.displayFour(sqlQueries.changeBus("Bus10","Trip9","11-9-2001","SUN 10:00AM"));
-                break;
+                queryFive();
+                 break;
             case "6":
-                //query 3
-                displayResults.displayFive(sqlQueries.displayStops());
+                querySix();
                 break;
             case "7":
-                //query 4
-                displayResults.displaySix(sqlQueries.displayWeeklySchedule("Driver7","1-7-2001"));
+                querySeven();
                 break;
             case "8":
-                //query 5
-                System.out.println("Query 5 Results");
-                displayResults.displayThree(sqlQueries.addDriver("jamz","909-999-8888"));
+                queryEight();
                 break;
             case "9":
-                //query 6
-                System.out.println("Query 6 Results");
-                displayResults.displayThree((sqlQueries.addBus("Bus9999","Tesla","2020")));
+                queryNine();
                 break;
             case "10":
-                //query 7
-                System.out.println("Query 7 Results");
-                displayResults.displayTwo(sqlQueries.deleteBus("Bus999"));
+                queryTen();
                 break;
             case "11":
-                //query 8
-                // Can't change due to foreign key constraint
-                displayResults.displayThree(sqlQueries.addActualTrip("Trip2","1-2-2001","TUE 10:00AM","Stop1","TUE 2:45PM","1","1", "1", "1"));
+                queryEleven();
                 break;
             case "12":
                 System.exit(0);
@@ -110,4 +95,74 @@ public class TerminalMenu {
 
         }
     }
+
+    /*
+        TODO: Replace hard coded values in the methods below with user input. a Scanner is already defined too, named: scanner
+     */
+
+    private void queryOne() throws SQLException {
+        // query 1
+        System.out.println("Query 1 Results");
+        displayResults.display(sqlQueries.displaySchedule("Pomona", "Walnut", "1-1-2001"));
+
+    }
+
+    private void queryTwo() throws SQLException {
+        // query 2.a
+        System.out.println("Query 2 Results");
+        displayResults.displayTwo(sqlQueries.deleteTrip("Trip1","1-1-2001","MON 10:00AM"));
+
+
+    }
+    private void queryThree() throws SQLException {
+        //query 2.b
+        displayResults.displayThree(sqlQueries.addTrip("Trip1","11-21-2017","MON 10:55AM","MON 12:00PM","Driver1","Bus2"));
+
+    }
+    private void queryFour() throws SQLException {
+        //query 2.c
+        displayResults.displayFour(sqlQueries.changeDriver("Driver1","Trip10","1-10-2001","SUN 10:00AM"));
+
+    }
+    private void queryFive() throws SQLException {
+        //query 2.d
+        displayResults.displayFour(sqlQueries.changeBus("Bus10","Trip9","11-9-2001","SUN 10:00AM"));
+
+    }
+    private void querySix() throws SQLException {
+        //query 3
+        displayResults.displayFive(sqlQueries.displayStops());
+
+    }
+    private void querySeven() throws SQLException {
+        //query 4
+        displayResults.displaySix(sqlQueries.displayWeeklySchedule("Driver7","1-7-2001"));
+
+    }
+    private void queryEight() throws SQLException {
+        //query 5
+        System.out.println("Query 5 Results");
+        displayResults.displayThree(sqlQueries.addDriver("jamz","909-999-8888"));
+
+    }
+    private void queryNine() throws SQLException {
+        //query 6
+        System.out.println("Query 6 Results");
+        displayResults.displayThree((sqlQueries.addBus("Bus9999","Tesla","2020")));
+
+    }
+    private void queryTen() throws SQLException {
+        //query 7
+        System.out.println("Query 7 Results");
+        displayResults.displayTwo(sqlQueries.deleteBus("Bus999"));
+
+    }
+    private void queryEleven() throws SQLException {
+        //query 8
+        // Can't change due to foreign key constraint
+        displayResults.displayThree(sqlQueries.addActualTrip("Trip2","1-2-2001","TUE 10:00AM","Stop1","TUE 2:45PM","1","1", "1", "1"));
+
+    }
+
+
 }
